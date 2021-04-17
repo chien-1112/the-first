dfb9dbf9d83bab684359d64fda44fbab5fcafc98
chien-1112 <82670070+chien-1112@users.noreply.github.com>
2021-04-17 07:13:04 +0700

    commit

---
+++ chien.py
@@ -0,0 +1,10 @@
+a=115
+
+if a < 10:
+	print("young")
+elif a < 20: 
+	print("pre-adult")
+	if a >= 15 and a <= 18:
+		print("student")
+else:
+	print("adult")
---
+++ hello.py
@@ -0,0 +1,5 @@
+print("nhap USD: ")
+usd = input()
+vnd = int(usd) * 22 
+print(str(usd)+ " USD=" + str(vnd) + "k VND")
+
